7e145d46102cfe34e1acae201d8546ca13fade1a
profe <profe@Ti4>
2014-02-10 17:37:11 +0100

    final

--- TitularsDeNoticies/gen/net/infobosccoma/titularsnoticies/R.java
+++ TitularsDeNoticies/gen/net/infobosccoma/titularsnoticies/R.java
@@ -10,6 +10,10 @@ package net.infobosccoma.titularsnoticies;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int verdclar=0x7f040000;
+        public static final int vermell=0x7f040001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,25 +21,30 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int LinearLayout1=0x7f090000;
+        public static final int action_settings=0x7f090004;
+        public static final int listTitulars=0x7f090001;
+        public static final int txtViewSubtitol=0x7f090003;
+        public static final int txtViewTitol=0x7f090002;
     }
     public static final class layout {
         public static final int activity_titulars=0x7f030000;
+        public static final int listitem_titular=0x7f030001;
     }
     public static final class menu {
-        public static final int titulars=0x7f070000;
+        public static final int titulars=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
     }
     public static final class style {
         /** 
@@ -59,10 +68,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- TitularsDeNoticies/src/net/infobosccoma/titularsnoticies/TitularsActivity.java
+++ TitularsDeNoticies/src/net/infobosccoma/titularsnoticies/TitularsActivity.java
@@ -1,15 +1,35 @@
 package net.infobosccoma.titularsnoticies;
 
-import android.os.Bundle;
+import net.infobosccoma.titularsnoticies.model.Titular;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
 
-public class TitularsActivity extends Activity {
-
+public class TitularsActivity extends Activity implements OnItemClickListener {
+	private TitularsAdapter adapter;
+	private Titular[] dades;
+	private ListView llista;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_titulars);
+		
+		dades = new Titular[10];
+		dades[0] = new Titular("El Barça no és líder", "Va perdre estrepitosament amb el València");
+		dades[1]= new Titular("Aniol suspès", "Ha suspès per ser del Madrid");
+		for(int i = 2; i < 10; i++) {
+			dades[i] = new Titular("Titol " + (i+1), "Subtitol " + (i+1));
+		}
+		
+		adapter = new TitularsAdapter(this, dades);
+		
+		llista = ((ListView)findViewById(R.id.listTitulars));
+		llista.setAdapter(adapter);
+		llista.setOnItemClickListener(this);
 	}
 
 	@Override
@@ -19,4 +39,45 @@ public class TitularsActivity extends Activity {
 		return true;
 	}
 
+	@Override
+	public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+		Dialeg dialeg = new Dialeg();
+
+		dialeg.setMissatge(((Titular)adapter.getItem(position)).getTitol());
+		
+		dialeg.show(getFragmentManager(),"" );	
+		
+	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
